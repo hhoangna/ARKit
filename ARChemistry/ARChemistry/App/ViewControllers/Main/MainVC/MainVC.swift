c057db3bf78a1404f6948d64946abf5494e891a5
Mach Van Nguyen <nguyen@horical.com>
2018-06-28 22:01:02 +0700

    update code

--- ARChemistry/ARChemistry/App/ViewControllers/Main/MainVC/MainVC.swift
+++ ARChemistry/ARChemistry/App/ViewControllers/Main/MainVC/MainVC.swift
@@ -29,7 +29,7 @@ class MainVC: BaseVC {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        showSlideMenu(isShow: false, animation: false)
+        showSlideMenu(isShow: true, animation: false)
         pushWorkingListToMain()
         setUpPanGesture()
     }
